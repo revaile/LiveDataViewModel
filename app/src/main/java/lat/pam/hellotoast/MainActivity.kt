b779d1cce715b51f2cc93f4f58c21021bd6c1353
IrmaDwiyanti <irmadwiyanti.sky.@gmail.com>
2024-09-26 13:16:29 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/hellotoast/MainActivity.kt
@@ -0,0 +1,59 @@
+package lat.pam.hellotoast
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+
+class MainActivity : AppCompatActivity() {
+    private var mCount = 0;
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("test debug","Hallo")
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val mShowCount = findViewById<TextView>(R.id.show_count)
+        val buttonCountUp = findViewById<Button>(R.id.button_count)
+        val buttonToast = findViewById<Button>(R.id.button_toast)
+        val buttonSwitchPage = findViewById<Button>(R.id.button_switchpage)
+        val buttonBrowser = findViewById<Button>(R.id.button_browser)
+
+        buttonCountUp.setOnClickListener(View.OnClickListener {
+            mCount++;
+            if (mShowCount != null)
+                mShowCount.text = mCount.toString()
+        })
+        buttonToast.setOnClickListener(View.OnClickListener {
+            val tulisan: String = mShowCount?.text.toString()
+            val toast: Toast = Toast.makeText(this, "Angka yang dimunculkan "+tulisan, Toast.LENGTH_LONG)
+            toast.show()
+        })
+        buttonSwitchPage.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        })
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        buttonBrowser.setOnClickListener(View.OnClickListener {
+            val intentbrowse = Intent(Intent.ACTION_VIEW)
+            intentbrowse.setData(Uri.parse("https://www.google.com/"))
+            startActivity(intentbrowse)
+        })
+
+    }
+}
